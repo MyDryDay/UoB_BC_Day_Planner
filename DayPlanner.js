ac78f9a8ea5b7b742c24d4b6f6c838f4fb43c1ff
George Cope <G.Cope13@gmail.com>
2021-01-14 15:43:32 +0000

    Created some variables for the time of day & hour of the day. Created an if/else statement nested inside a for loop to determine what the background colour of each timeslot element should be based on the time of day

--- DayPlanner.js
+++ DayPlanner.js
@@ -5,6 +5,13 @@ $(document).ready(function(){
     var currentDate = luxon.DateTime.local();
     $("#currentDay").text("Today's date is: " + currentDate.toLocaleString());
 
+    // The following variable will get the current time based on the user's system
+    var currentTime = currentDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
+    console.log(currentTime);
+    // The following variable will get the current hour based on the user's system
+    var currentHour = currentDate.hour;
+    console.log(currentHour);
+
     // Adds a new <div> element with the id of "timeBlocks" to the <div> with a class of "container"
     // This will be where all the individual timeblocks will be displayed
     var timeBlocks = "<div id = 'timeBlocks' class = 'time-block'></div>"
@@ -25,16 +32,34 @@ $(document).ready(function(){
         var timeSlot = "<div id = " + blocks[i] + " class = 'row hour'></div>";
         $("#timeBlocks").append(timeSlot);
 
-        var textInput = "<input type = 'text' id = 'textInput' class = 'col-md-11' name = 'textInput'>";
+        var textInput = "<input type = 'text' id = 'textInput' class = '" + blocks[i] + " col-md-11 col-sm-11' name = 'textInput'>";
         $("#" + blocks[i]).append(textInput);
 
-        var saveBtn = "<input type = 'button' id = 'saveBtn' class = 'saveBtn col-md-1'>"
+        var saveBtn = "<input type = 'button' id = 'saveBtn' class = '" + blocks[i] + " saveBtn col-md-1 col-sm-1'>"
         $("#" + blocks[i]).append(saveBtn);
+        
     }
 
-    // The following variable will get the current time based on the user's system
-    var currentTime = currentDate.toLocaleString(luxon.DateTime.TIME_SIMPLE);
-    console.log(currentTime);
+    // The following loop compares the value of each index from "blocks" (which were previously assigned as classes to each timeslot)
+    // with the currentHour
+    // Depending on whether currentHour is greater than, lesser than or equal to the value of each index
+    // the background colour of the element will change.
+    // This allows the background colour for past, present and future timeslots to be different.
+    for(var i = 0; i < blocks.length; i++){
+        // console.log(blocks[i]);
 
+        if(currentHour > blocks[i]){
+            // console.log(blocks[i]);
+            $("." + blocks[i]).addClass("past");
+        } else if(currentHour < blocks[i]){
+            // console.log(blocks[i]);
+            $("." + blocks[i]).addClass("future");
+        } else if(currentHour == blocks[i]){
+            // console.log(blocks[i]);
+            $("." + blocks[i]).addClass("present");
+        } else{
+            console.log("nope");
+        }
+    }
     
 })
