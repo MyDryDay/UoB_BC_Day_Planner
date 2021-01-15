c524f55a796f9529baddb1fdc5c7936324f050b6
George Cope <G.Cope13@gmail.com>
2021-01-15 16:37:12 +0000

    Rewritten the code that created HTML elements to create them with unique ID's, as before many of the had the same ID

--- DayPlanner.js
+++ DayPlanner.js
@@ -20,60 +20,81 @@ $(document).ready(function(){
     // Each number in the array coresponds to a particular timeslot
     // 7 = 7am - 8am, 13 = 1pm - 2pm, 18 = 6pm - 7pm etc.
     var blocks = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-    // This loops creates a series of <div>s, each with an id coresponding to
-    // one of the values in the "blocks" variable.
-    // It then creates a textfield input & an input button and appends them to the aforementioned <div>s.
     for(var i = 0; i < blocks.length; i++){
         // var timeSlot = "<div class = 'timeSlot'></div>";
         // console.log(blocks[i]);
         // $(".timeSlot").attr("id", blocks[i]);
         // $("#timeBlocks").append(timeSlot);
 
-        var timeSlot = "<div id = " + blocks[i] + " class = 'row hour'></div>";
+        // Here a series of <div>s are created, each with an id coresponding to one of the values in the "blocks" variable.
+        // It then creates a textfield input & an input button and appends them to the aforementioned <div>s.
+        var timeSlot = "<div id = timeSlot" + blocks[i] + " class = 'row hour'></div>";
         $("#timeBlocks").append(timeSlot);
 
-        var time = "<p class = 'hour col-md-1 col-sm-1 " + blocks[i] + "'>" + blocks[i] + "</p>"
-        $("#" + blocks[i]).append(time);
+        var time = "<p id = 'time" + blocks[i] + "'class = 'hour col-md-1 col-sm-1'>" + blocks[i] + "</p>"
+        $("#timeSlot" + blocks[i]).append(time);
 
-        var textInput = "<input type = 'text' id = 'textInput' class = '" + blocks[i] + " col-md-10 col-sm-10' name = 'textInput'>";
-        $("#" + blocks[i]).append(textInput);
+        var textInput = "<input type = 'text' id = 'textInput input" + blocks[i] + "' class = 'col-md-10 col-sm-10' name = 'textInput'>";
+        $("#timeSlot" + blocks[i]).append(textInput);
 
-        var saveBtn = "<input type = 'button' id = 'saveBtn' class = '" + blocks[i] + " saveBtn col-md-1 col-sm-1'>"
-        $("#" + blocks[i]).append(saveBtn);
+        var saveBtn = "<input type = 'button' id = 'save" + blocks[i] + "' class = 'saveBtn col-md-1 col-sm-1'>"
+        $("#timeSlot" + blocks[i]).append(saveBtn);
   
-    }
-
-    // The following will append either "AM" or "PM" to the end of the text within the first <p> child of any element.
-    // What will be appended is decided by whether or not it's timeslot is less than or equal to 11 (for "AM") and greater than or equal to 12 (for "PM")
-    if(blocks[i] <= 11){
-        $("p:first-child").append("AM");
-    } else{
-        $("p:first-child").append("PM");
-    }
-
-    // The following loop compares the value of each index from "blocks" (which were previously assigned as classes to each timeslot)
-    // with the currentHour
-    // Depending on whether currentHour is greater than, lesser than or equal to the value of each index
-    // the background colour of the element will change.
-    // This allows the background colour for past, present and future timeslots to be different.
-    for(var i = 0; i < blocks.length; i++){
-        // console.log(blocks[i]);
+        // The following will append either "AM" or "PM" to the end of the text within the first <p> child of any element.
+        // What will be appended is decided by whether or not it's timeslot is less than or equal to 11 (for "AM") and greater than or equal to 12 (for "PM")
+        if(blocks[i] <= 11){
+            $("#time" + blocks[i]).append("AM");
+        } else if(blocks[i] >= 12){
+            $("#time" + blocks[i]).append("PM");
+        }
 
         if(currentHour > blocks[i]){
             // console.log(blocks[i]);
-            $("." + blocks[i]).addClass("past");
+            $("#input" + blocks[i]).addClass("past");
         } else if(currentHour < blocks[i]){
             // console.log(blocks[i]);
-            $("." + blocks[i]).addClass("future");
+            $("#input" + blocks[i]).addClass("future");
         } else if(currentHour == blocks[i]){
             // console.log(blocks[i]);
-            $("." + blocks[i]).addClass("present");
+            $("#input" + blocks[i]).addClass("present");
         } else{
             console.log("nope");
         }
+        
     }
 
-    
+
+    // // The following loop compares the value of each index from "blocks" (which were previously assigned as classes to each timeslot)
+    // // with the currentHour
+    // // Depending on whether currentHour is greater than, lesser than or equal to the value of each index
+    // // the background colour of the element will change.
+    // // This allows the background colour for past, present and future timeslots to be different.
+    // for(var i = 0; i < blocks.length; i++){
+    //     // console.log(blocks[i]);
+
+    //     if(currentHour > blocks[i]){
+    //         // console.log(blocks[i]);
+    //         $("." + blocks[i]).addClass("past");
+    //     } else if(currentHour < blocks[i]){
+    //         // console.log(blocks[i]);
+    //         $("." + blocks[i]).addClass("future");
+    //     } else if(currentHour == blocks[i]){
+    //         // console.log(blocks[i]);
+    //         $("." + blocks[i]).addClass("present");
+    //     } else{
+    //         console.log("nope");
+    //     }
+    // }
+
+    // $("#saveBtn").click(function(event){
+    //     event.preventDefault();
+        
+    //     for(i = 0; i < blocks.length; i++){
+    //         var inputText = $("[#textInput][." + blocks[i] + "]").val();
+    //         console.log(inputText);
+    //     }
+
+    // });
 })
 
 
